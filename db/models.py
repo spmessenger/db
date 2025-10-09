d8ed20fa7ec46c8fde18ad52de6ec2382c0b707e
unknown <ro9aine@gmail.com>
2025-10-09 16:25:03 +0300

    feat

--- db/models.py
+++ db/models.py
@@ -1,8 +1,14 @@
+from sqlalchemy import String, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 
 class User(Base):
     __tablename__ = 'users'
+
+    username: Mapped[str] = mapped_column(String(16), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(128))
+    refresh_tokens: Mapped[list[str]] = mapped_column(ARRAY(String(128)))
 
 
 class Message(Base):
