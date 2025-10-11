0fa60a09716d8203e8aa38a73b572f1422af0519
unknown <ro9aine@gmail.com>
2025-10-11 16:49:37 +0300

    fat

--- db/misc/tables.py
+++ db/misc/tables.py
@@ -1,9 +1,13 @@
+from sqlalchemy import inspect
 from ..base import Base
 from ..session import engine
 
 
-def create_tables():
+def create_tables() -> list[str]:
     Base.metadata.create_all(engine)
+
+    inspector = inspect(engine)
+    return inspector.get_table_names()
 
 
 def drop_tables():
